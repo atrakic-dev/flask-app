90f8e99d8801c40c9cc1cafca648978162022f0b
Admir Trakic <atrakic@users.noreply.github.com>
2019-10-29 18:11:23 +0100

    Toggle false

--- main.tf
+++ main.tf
@@ -1,23 +1,27 @@
-locals {
-  enabled = var.enabled == "true" ? true : false
-  name    = "flask-app"
-}
-
 variable "enabled" {
   description = "Set to false to prevent the module from creating any resources"
-  default     = "true"
+  default     = "false"
+}
+
+locals {
+  enabled  = var.enabled == "true" ? true : false
+  name     = "flask-app"
+  ssh_keys = [25624561]
+  image    = "ubuntu-18-04-x64"
+  region   = "ams3"
+  size     = "s-1vcpu-1gb"
 }
 
 provider "digitalocean" {}
 
 resource "digitalocean_droplet" "docker-compose-server" {
   count     = local.enabled == true ? 1 : 0
-  image     = "ubuntu-18-04-x64"
-  region    = "ams3"
-  size      = "s-1vcpu-1gb"
+  image     = local.image
+  region    = local.region
+  size      = local.size
   ipv6      = true
   name      = format("%s-%d", local.name, count.index)
-  ssh_keys  = [25624561]
+  ssh_keys  = local.ssh_keys
   user_data = file("${path.module}/userdata.sh")
   tags      = ["terraform"]
 }
