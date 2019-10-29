4f1b9b1e45c5839a8df3d8639eda6cb56da48aa9
Admir Trakic <atrakic@users.noreply.github.com>
2019-10-29 17:55:26 +0100

    Add toggle

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 locals {
-  name     = "flask-app"
-  region   = "ams3"
+  enabled = var.enabled == "true" ? true : false
+  name    = "flask-app"
+  region  = "ams3"
+}
+
+variable "enabled" {
+  description = "Set to false to prevent the module from creating any resources"
+  default     = "false"
 }
 
 provider "digitalocean" {}
 
 resource "digitalocean_droplet" "docker-compose-server" {
+  count     = local.enabled == true ? 1 : 0
   image     = "ubuntu-18-04-x64"
   region    = "ams3"
   size      = "s-1vcpu-1gb"
