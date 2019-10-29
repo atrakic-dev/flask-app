9a191cf12ec25926312f2d1745d05d22db4843a3
Admir Trakic <atrakic@users.noreply.github.com>
2019-10-29 17:39:58 +0100

    Try with user-data

--- main.tf
+++ main.tf
@@ -1,31 +1,25 @@
 locals {
-  name   = "flask-app"
-  region = "ams3"
+  name     = "flask-app"
+  region   = "ams3"
+  ssh_keys = var.ssh_keys
+}
+
+variable "ssh_keys" {
+  description = "Obtain your ssh_key id number via your account: https://developers.digitalocean.com/documentation/v2/#list-all-key"
+  type        = list(string)
 }
 
 provider "digitalocean" {}
 
-resource "digitalocean_droplet" "my-docker-server" {
-  image  = "ubuntu-18-04-x64"
-  region = "ams3"
-  size   = "s-1vcpu-1gb"
-  ipv6   = true
-  name   = format("%s-%s", local.name, local.region)
-
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin:/usr/local/bin",
-      "sudo apt-get update",
-      "sudo apt-get dist-upgrade -yy",
-      "sudo apt-get -y install git curl sudo",
-      "sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose",
-      "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
-      "git clone -q https://github.com/atrakic-dev/flask-app.git",
-      "cd flask-app && sudo docker-compose up -d"
-    ]
-  }
-  tags = ["terraform"]
+resource "digitalocean_droplet" "docker-compose-server" {
+  image     = "ubuntu-18-04-x64"
+  region    = "ams3"
+  size      = "s-1vcpu-1gb"
+  ipv6      = true
+  name      = format("%s-%s", local.name, local.region)
+  ssh_keys  = local.ssh_keys
+  user_data = file("${path.module}/userdata.sh")
+  tags      = ["terraform"]
 }
 
 output "hello_world" {
