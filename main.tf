2428bdcb8239417df6c90014d8fec21944df3104
Admir Trakic <atrakic@users.noreply.github.com>
2019-10-29 15:42:02 +0100

    Update

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = ">= 0.12.10"
   backend "remote" {
     organization = "xomodo"
     workspaces {
       name = "flask-app"
     }
   }
+}
+
+output "hello" {
+  value = "World"
 }
